ab7d2e9569a6a3e9bd52ad7b4246ef99934d4d14
rosyiid1440 <abdurrosyiid_170602@umg.ac.id>
2022-09-16 15:12:30 +0700

    tidak tahu apakah salah

--- promise.js
+++ promise.js
@@ -1,7 +1,18 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi) => { 
+  let hasilemosi = 0;
+  try {
+    let data1 = await promiseTheaterIXX();
+    let data2 = await promiseTheaterVGC();
+    data1.map((item) => { (item.hasil === emosi) ? hasilemosi += 1 : 0 });
+    data2.map((item) => { (item.hasil === emosi) ? hasilemosi += 1 : 0 });
+    return hasilemosi;
+  } catch (error) {
+    return error;
+  }
+};
 
 module.exports = {
   promiseOutput,
